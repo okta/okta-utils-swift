3f0a91ba374b5c83ec7494994faa27c3322700ed
Steve Lind <64522202+stevelind-okta@users.noreply.github.com>
2020-05-18 16:33:36 -0700

    Create shared logger for objective-c macros

--- OktaLogger/OktaLogger+Helpers.h
+++ OktaLogger/OktaLogger+Helpers.h
@@ -1,19 +1,10 @@
-@import Foundation;
-@class OktaLogger;
+#define okl_debug(e, s, ... ) [OktaLogger.shared debugWithEventName:e message:[NSString stringWithFormat:(s), ##__VA_ARGS__] properties:nil file:[NSString stringWithUTF8String:__FILE__] line:@(__LINE__) funcName:[NSString stringWithUTF8String:__PRETTY_FUNCTION__]];
 
-/*
- Get/Set the logger instance used for Objective-C Macros
- */
-OktaLogger *DefaultLogger(void);
-void setDefaultLogger(OktaLogger *logger);
+#define okl_info(e, s, ... ) [OktaLogger.shared infoWithEventName:e message:[NSString stringWithFormat:(s), ##__VA_ARGS__] properties:nil file:[NSString stringWithUTF8String:__FILE__] line:@(__LINE__) funcName:[NSString stringWithUTF8String:__PRETTY_FUNCTION__]];
 
-#define okl_debug(e, s, ... ) [DefaultLogger() debugWithEventName:e message:[NSString stringWithFormat:(s), ##__VA_ARGS__] properties:nil file:[NSString stringWithUTF8String:__FILE__] line:@(__LINE__) funcName:[NSString stringWithUTF8String:__PRETTY_FUNCTION__]];
+#define okl_warn(e, s, ... ) [OktaLogger.shared warningWithEventName:e message:[NSString stringWithFormat:(s), ##__VA_ARGS__] properties:nil file:[NSString stringWithUTF8String:__FILE__] line:@(__LINE__) funcName:[NSString stringWithUTF8String:__PRETTY_FUNCTION__]];
 
-#define okl_info(e, s, ... ) [DefaultLogger() infoWithEventName:e message:[NSString stringWithFormat:(s), ##__VA_ARGS__] properties:nil file:[NSString stringWithUTF8String:__FILE__] line:@(__LINE__) funcName:[NSString stringWithUTF8String:__PRETTY_FUNCTION__]];
+#define okl_uievent(e, s, ... ) [OktaLogger.shared uiEventWithEventName:e message:[NSString stringWithFormat:(s), ##__VA_ARGS__] properties:nil file:[NSString stringWithUTF8String:__FILE__] line:@(__LINE__) funcName:[NSString stringWithUTF8String:__PRETTY_FUNCTION__]];
 
-#define okl_warn(e, s, ... ) [DefaultLogger() warningWithEventName:e message:[NSString stringWithFormat:(s), ##__VA_ARGS__] properties:nil file:[NSString stringWithUTF8String:__FILE__] line:@(__LINE__) funcName:[NSString stringWithUTF8String:__PRETTY_FUNCTION__]];
-
-#define okl_uievent(e, s, ... ) [DefaultLogger() uiEventWithEventName:e message:[NSString stringWithFormat:(s), ##__VA_ARGS__] properties:nil file:[NSString stringWithUTF8String:__FILE__] line:@(__LINE__) funcName:[NSString stringWithUTF8String:__PRETTY_FUNCTION__]];
-
-#define okl_error(e, s, ... ) [DefaultLogger() errorWithEventName:e message:[NSString stringWithFormat:(s), ##__VA_ARGS__] properties:nil file:[NSString stringWithUTF8String:__FILE__] line:@(__LINE__) funcName:[NSString stringWithUTF8String:__PRETTY_FUNCTION__]];
+#define okl_error(e, s, ... ) [OktaLogger.shared errorWithEventName:e message:[NSString stringWithFormat:(s), ##__VA_ARGS__] properties:nil file:[NSString stringWithUTF8String:__FILE__] line:@(__LINE__) funcName:[NSString stringWithUTF8String:__PRETTY_FUNCTION__]];
 

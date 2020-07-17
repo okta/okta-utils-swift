c8a553849c459adde995e508f1ed654da659dbf5
Kaushik Krishnakumar <kaushik.krishnakumar@okta.com>
2020-07-16 20:57:05 -0700

    Bugfix Logging levels - Selecting a log level doesn't print logs more serious than selected level.
    bug: when info is selected, logger doesnt print Warning, UI event, error logs though they are higher priority.
    fix: store union set value for each log level

--- OktaLogger/OktaLoggerLogLevel.swift
+++ OktaLogger/OktaLoggerLogLevel.swift
@@ -12,11 +12,12 @@ public class OktaLoggerLogLevel: NSObject, OptionSet {
         self.rawValue = rawValue
     }
     @objc public static let off = OktaLoggerLogLevel([])
-    @objc public static let debug = OktaLoggerLogLevel(rawValue: 1 << 0)
-    @objc public static let info = OktaLoggerLogLevel(rawValue: 1 << 1)
-    @objc public static let warning = OktaLoggerLogLevel(rawValue: 1 << 2)
-    @objc public static let uiEvent = OktaLoggerLogLevel(rawValue: 1 << 3)
     @objc public static let error = OktaLoggerLogLevel(rawValue: 1 << 4)
+    @objc public static let uiEvent:OktaLoggerLogLevel = [OktaLoggerLogLevel(rawValue: 1 << 3), .error]
+    @objc public static let warning:OktaLoggerLogLevel = [OktaLoggerLogLevel(rawValue: 1 << 2),.uiEvent]
+    @objc public static let info:OktaLoggerLogLevel = [OktaLoggerLogLevel(rawValue: 1 << 1), .warning]
+    @objc public static let debug: OktaLoggerLogLevel = [OktaLoggerLogLevel(rawValue: 1 << 0), .info]
+    
     @objc public static let all: OktaLoggerLogLevel = [.debug, .info, .warning, .uiEvent, .error]
 }
 

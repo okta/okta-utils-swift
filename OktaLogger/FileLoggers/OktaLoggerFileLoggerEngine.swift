32b32bc8a9ceb8bef5e708d2dabdfaf48a782020
Kaushik Krishnakumar <kaushik.krishnakumar@okta.com>
2020-07-29 16:09:50 -0700

    Refactored classes, unit tests

--- OktaLogger/FileLoggers/OktaLoggerFileLoggerEngine.swift
+++
@@ -1,9 +0,0 @@
-//
-// Created by Kaushik Krishnakumar on 7/29/20.
-// Copyright (c) 2020 Okta, Inc. All rights reserved.
-//
-
-
-enum OktaLoggerFileLoggerEngine {
-    case CocoaLumberjack
-}
--- OktaLoggerTests/OktaLoggerFileLoggerTests.swift
+++ OktaLoggerTests/OktaLoggerFileLoggerTests.swift
@@ -11,17 +11,16 @@ import XCTest
 @testable import OktaLogger
 
 class OktaLoggerFileLoggerTests: XCTestCase {
+    let TWO_DAYS = TimeInterval(48 * 60 * 60)
 
-    func testFileLogger() {
+    func testOktaFileLogger() {
         let testObject: OktaLoggerFileLogger = OktaLoggerFileLogger(identifier: "hello.world", level: .all, defaultProperties: nil)
         let logger = OktaLogger(destinations: [testObject])
         XCTAssertEqual(testObject.logsCanBePurged(), true)
-        // default rolling frequency
-        XCTAssertEqual(testObject.fileLogger.rollingFrequency, 48 * 60 * 60)
         for i in 1...5 {
             logger.debug(eventName: "BEFORE_PURGE", message: "\(i):log message")
         }
-        print(testObject.logDirectoryAbsolutePath()!)
+
         var logs = testObject.getLogs()
         var data = logs[0] as Data
         let lineCount = countLines(data)
@@ -30,6 +29,35 @@ class OktaLoggerFileLoggerTests: XCTestCase {
 
         logger.debug(eventName: "AFTER_PURGE", message: "Debug log")
         logger.info(eventName: "AFTER_PURGE", message: "Debug log")
+        // new logs dont get immediately to disk written after rolling. We can force flush destination to write to file. Or wait few moments
+        logs = testObject.getLogs()
+        data = logs[0] as Data
+        let newLineCount = countLines(data)
+        XCTAssertEqual(newLineCount, 2)
+    }
+
+    func testLumberjackFileLogger() {
+
+        let logConfig = OktaLoggerFileLoggerConfig(rollingFrequency: TWO_DAYS)
+        let testObject = LumberjackLoggerDelegate(logConfig)
+
+        // default rolling frequency
+        XCTAssertEqual(testObject.fileLogger.rollingFrequency, TWO_DAYS)
+        XCTAssertNotNil(testObject.dirPath())
+
+        XCTAssertEqual(testObject.logsCanBePurged(), true)
+        for i in 1...5 {
+            testObject.log(.debug, "log \(i)")
+        }
+
+        var logs = testObject.getLogs()
+        var data = logs[0] as Data
+        let lineCount = countLines(data)
+        XCTAssertEqual(lineCount, 5)
+        testObject.purgeLogs()
+
+        testObject.log(.debug, "After purge")
+        testObject.log(.info, "After purge")
         // new logs dont get immediately to disk written after rolling. We can force flush destination to write to file. Or wait few moments
         logs = testObject.getLogs()
         data = logs[0] as Data

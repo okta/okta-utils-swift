c1dcd19b2f174b76e39d61db0b37aada6bb2b720
Marc Powell <marc.powell@okta.com>
2021-07-01 20:36:27 +0000

    Preparation for open source repo
    OKTA-404190
    <<<Jenkins Check-In of Tested SHA: 5c10baa583b783f4fdee453081f851325e8b4bc0 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-ios-logger
    Files changed count: 39
    PR Link: "https://github.com/okta/okta-logger-swift/pull/39"

--- OktaLogger/AppCenterLogger/AppCenterLogger.swift
+++ OktaLogger/AppCenterLogger/AppCenterLogger.swift
@@ -1,8 +1,14 @@
-//
-// Created by Brenner Ryan on 5/11/21.
-// Copyright (c) 2021 Okta, Inc. All rights reserved.
-//
-
+/*
+ * Copyright (c) 2020-Present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 import AppCenterAnalytics
 
 /**

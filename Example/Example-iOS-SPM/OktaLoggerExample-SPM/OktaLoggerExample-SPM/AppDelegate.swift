daf384589f59ce3fea66848c3bec5283276a6f4d
Moises Olmedo <moises.olmedopina@okta.com>
2022-05-06 13:54:20 -0400

    Adding SPM example CI

--- Example/Example-iOS-SPM/OktaLoggerExample-SPM/OktaLoggerExample-SPM/AppDelegate.swift
+++ Example/Example-iOS-SPM/OktaLoggerExample-SPM/OktaLoggerExample-SPM/AppDelegate.swift
@@ -15,8 +15,6 @@ import LoggerCore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let logger = OktaLogger()

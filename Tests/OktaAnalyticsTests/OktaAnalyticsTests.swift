c0510d7b165f901019b58453d519a15e67e0a0f1
Artem Shvydkyi <artem.shvydkyi@okta.com>
2024-02-06 13:45:57 +0100

    Add unit test

--- Tests/OktaAnalyticsTests/OktaAnalyticsTests.swift
+++ Tests/OktaAnalyticsTests/OktaAnalyticsTests.swift
@@ -17,24 +17,6 @@ import AppCenterAnalytics
 
 class OktaAnalyticsTests: XCTestCase {
 
-    override class func setUp() {
-        let appCenterAnalyticsProvider: AnalyticsProviderProtocol = {
-            let logger = OktaLogger()
-            logger.addDestination(
-                OktaLoggerConsoleLogger(
-                    identifier: "com.okta.loggerDemo.console",
-                    level: OktaLoggerLogLevel.debug,
-                    defaultProperties: nil
-                )
-            )
-            let appCenterAnalyticsProvider = AppCenterAnalyticsProvider(name: "AppCenter", logger: logger)
-            appCenterAnalyticsProvider.start(withAppSecret: "App Secret", services: [AppCenterAnalytics.Analytics.self])
-            return appCenterAnalyticsProvider
-        }()
-        OktaAnalytics.initializeStorageWith(securityAppGroupIdentifier: "")
-        OktaAnalytics.addProvider(appCenterAnalyticsProvider)
-    }
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -44,6 +26,7 @@ class OktaAnalyticsTests: XCTestCase {
     }
 
     func testScenario() throws {
+        setupOktaAnalytics()
         let count = 30
         var insertExpectations = [XCTestExpectation]()
         var deleteExpectations = [XCTestExpectation]()
@@ -81,6 +64,7 @@ class OktaAnalyticsTests: XCTestCase {
     }
 
     func testScenarioWithScenarioIDFromClient() throws {
+        setupOktaAnalytics()
         let count = 30
         var insertExpectations = [XCTestExpectation]()
         var deleteExpectations = [XCTestExpectation]()
@@ -117,6 +101,51 @@ class OktaAnalyticsTests: XCTestCase {
         wait(for: insertExpectations + deleteExpectations, timeout: 40)
     }
 
+    func testExpiredScenarioEvents() throws {
+
+        setupOktaAnalytics(expirationPeriodForScenarioEvents: 3)
+        
+        let fetchedExpiredEventsExpectation = XCTestExpectation(description: "Fetched expired events")
+        let emptyExpiredEventsExpectation = XCTestExpectation(description: "No any expired events")
+        var scenarioIDs = Set<String>()
+        let count = 5
+        for i in 0..<count {
+            var scenarioID: String = ""
+            OktaAnalytics.startScenario(ScenarioEvent(name: "Test \(i)", displayName: " \(i)")) {
+                scenarioID = $0 ?? ""
+            }
+            Thread.sleep(forTimeInterval: 0.5)
+            
+            OktaAnalytics.updateScenario(scenarioID, [Property(key: "Test1", value: "1")])
+            OktaAnalytics.updateScenario(scenarioID, [Property(key: "Test2", value: "2")])
+            OktaAnalytics.updateScenario(scenarioID, [Property(key: "Test3", value: "3")])
+        
+            OktaAnalytics.getScenarioEventByID(scenarioID) {
+                XCTAssertNotNil($0)
+            }
+
+            scenarioIDs.insert(scenarioID)
+        }
+        Thread.sleep(forTimeInterval: 5)
+
+        OktaAnalytics.reportAndDeleteExpiredEvents { scenarioEvents in
+            let fetchedScenarioIDs = scenarioEvents.map {
+                $0.id
+            }
+            XCTAssertEqual(scenarioEvents.count, count)
+            XCTAssertEqual(scenarioIDs.subtracting(fetchedScenarioIDs).count, 0)
+            fetchedExpiredEventsExpectation.fulfill()
+        }
+        Thread.sleep(forTimeInterval: 1)
+        
+        OktaAnalytics.reportAndDeleteExpiredEvents { scenarioEvents in
+            XCTAssertEqual(scenarioEvents.count, 0)
+            emptyExpiredEventsExpectation.fulfill()
+        }
+
+        wait(for: [fetchedExpiredEventsExpectation, emptyExpiredEventsExpectation], timeout: 10)
+    }
+
     override class func tearDown() {
         OktaAnalytics.disposeAll()
     }
@@ -128,4 +157,22 @@ class OktaAnalyticsTests: XCTestCase {
         }
     }
 
+    private func setupOktaAnalytics(expirationPeriodForScenarioEvents: UInt? = nil) {
+        let appCenterAnalyticsProvider: AnalyticsProviderProtocol = {
+            let logger = OktaLogger()
+            logger.addDestination(
+                OktaLoggerConsoleLogger(
+                    identifier: "com.okta.loggerDemo.console",
+                    level: OktaLoggerLogLevel.debug,
+                    defaultProperties: nil
+                )
+            )
+            let appCenterAnalyticsProvider = AppCenterAnalyticsProvider(name: "AppCenter", logger: logger)
+            appCenterAnalyticsProvider.start(withAppSecret: "App Secret", services: [AppCenterAnalytics.Analytics.self])
+            return appCenterAnalyticsProvider
+        }()
+        OktaAnalytics.initializeStorageWith(securityAppGroupIdentifier: "",
+                                            expirationPeriodForScenarioEvents: expirationPeriodForScenarioEvents)
+        OktaAnalytics.addProvider(appCenterAnalyticsProvider)
+    }
 }
